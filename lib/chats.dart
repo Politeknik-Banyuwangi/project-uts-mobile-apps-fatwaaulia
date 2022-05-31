a4ec4c75405d61a2a9fccbc4412c9b8a22acfc15
FlutLab.io <>
2022-05-31 04:43:29 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -32,27 +32,9 @@ class Chats extends StatelessWidget {
               height: 10,
             ),
             ListTile(
-                leading: Stack(
-                  alignment: AlignmentDirectional.bottomEnd,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5fGKks_ZOlVCtiZQV6ETawu9L0xk74WbsvQ&usqp=CAU"),
-                      radius: 30,
-                    ),
-                    CircleAvatar(
-                      backgroundColor: Colors.green,
-                      radius: 10,
-                      child: Icon(
-                        Icons.add,
-                        size: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
                 title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                )),
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )),
             SizedBox(
               height: 10,
             ),
