755a3548aa04b9b5b6b12367fe568abc836aa83f
FlutLab.io <>
2022-05-31 05:52:24 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -5,6 +5,40 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class Calls extends StatelessWidget {
   const Calls({Key? key}) : super(key: key);
   // Masukkan coding disini
+  Widget getCalls(name, message, time) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage("https://i.ibb.co/80szBpR/fatwa.jpg"),
+        radius: 30,
+      ),
+      title: Row(
+        children: [
+          Text(
+            name,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+          ),
+          Spacer(),
+          Text(
+            time,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text(
+        message,
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +49,16 @@ class Calls extends StatelessWidget {
         },
         child: ListView(
           children: [
+            // getContainer("Recent updates"),
             SizedBox(
               height: 10,
             ),
-
-            // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
-            //
-            //
-            //
+            getCalls("Tiara TI D", "(2) Hari ini 11.19", "11.23"),
+            getCalls("Mbak Rindy YOT", "Kemarin 20.01", "11.23"),
+            getCalls("Bunbun", "19 Mei 06.56", "11.23"),
+            SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
