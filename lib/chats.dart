a33b4568230bf2075bf31d04d3b7ecbf741b09d2
FlutLab.io <>
2022-05-31 04:39:04 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -4,6 +4,20 @@ import 'package:flutter/material.dart';
 class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
 
+  Widget getStory(title, subTitle) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+        radius: 30,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+      ),
+      subtitle: Text(subTitle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,23 +31,43 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Stack(
-              alignment: AlignmentDirectional.bottomEnd,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5fGKks_ZOlVCtiZQV6ETawu9L0xk74WbsvQ&usqp=CAU"),
-                  radius: 30,
+            ListTile(
+                leading: Stack(
+                  alignment: AlignmentDirectional.bottomEnd,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5fGKks_ZOlVCtiZQV6ETawu9L0xk74WbsvQ&usqp=CAU"),
+                      radius: 30,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.green,
+                      radius: 10,
+                      child: Icon(
+                        Icons.add,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-                CircleAvatar(
-                  backgroundColor: Colors.green,
-                  radius: 10,
-                  child: Icon(
-                    Icons.add,
-                    size: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "My status",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Tap to add status update",
+                      style: TextStyle(color: Colors.grey.shade700),
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 10,
             ),
           ],
         ),
